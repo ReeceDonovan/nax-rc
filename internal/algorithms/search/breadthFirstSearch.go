ff11e6c704c25e3ac1d2db6d0a60ef83d2f466d6
Reece Donovan <reecedonovan99@gmail.com>
2023-04-07 10:01:01 +0100

    Implemented basic BFS for graph

---
+++ internal/algorithms/search/breadthFirstSearch.go
@@ -0,0 +1,30 @@
+package search
+
+import (
+	"github.com/ReeceDonovan/nax-rc/internal/dataStructures/graph"
+	"github.com/ReeceDonovan/nax-rc/internal/dataStructures/revlog"
+)
+
+func BreadthFirstSearch(g graph.Graph, startVertex graph.Vertex, id int) revlog.Revision {
+	visited := make(map[graph.Vertex]bool)
+	queue := []graph.Vertex{startVertex}
+
+	for len(queue) > 0 {
+		currentVertex := queue[0]
+		queue = queue[1:]
+
+		currentVertexHashcode := graph.GetElementHashcode(currentVertex)
+		if !visited[currentVertexHashcode] {
+			visited[currentVertexHashcode] = true
+			currentVertexData := currentVertex.(revlog.Revision)
+			if currentVertexData.ID() == id {
+				return currentVertexData
+			}
+			outgoingEdges := g.OutgoingEdges(currentVertex)
+			for _, edge := range outgoingEdges {
+				queue = append(queue, edge.TargetVertex())
+			}
+		}
+	}
+	return nil
+}
