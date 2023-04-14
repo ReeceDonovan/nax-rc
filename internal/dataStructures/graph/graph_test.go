4121f1234555a21fdcd4726af70fe8734c462c55
Reece Donovan <reecedonovan99@gmail.com>
2023-04-14 13:33:49 +0100

    Updated graph construction test

--- internal/dataStructures/graph/graph_test.go
+++ internal/dataStructures/graph/graph_test.go
@@ -1,9 +1,10 @@
 package graph
 
 import (
+	"fmt"
 	"testing"
 
-	"github.com/ReeceDonovan/nax-rc/internal/dataStructures/revlog"
+	"github.com/ReeceDonovan/nax-rc/internal/types"
 )
 
 // Benchmark adding a very large number of vertices and edges to a graph.
@@ -45,8 +46,8 @@ func BenchmarkGraphRemove(b *testing.B) {
 	}
 }
 
-func createGenericRevFromID(id int) revlog.Revision {
-	return revlog.NewBlankRevision(id)
+func createGenericRevFromID(id int) types.Revision {
+	return types.NewBlankRevision(id)
 }
 
 func BenchmarkGraphRev(b *testing.B) {
@@ -62,5 +63,31 @@ func BenchmarkGraphRev(b *testing.B) {
 		b.Logf("Vertices: %d", graph.vertices.Length())
 		b.Logf("Edges: %d", graph.edges.Length())
 		b.Logf("Graph:\n%s", graph.String())
+	}
+}
+
+const (
+	smallNumRevisions  = 20
+	mediumNumRevisions = 200
+	largeNumRevisions  = 2000
+	largeDataSize      = 1000000 // 1 Mb
+	mediumDataSize     = 1000    // 1 Kb
+	smallDataSize      = 100     // 100 bytes
+)
+
+// Create a benchmarks for constructing a graph of revisions with different sizes
+func BenchmarkGraph_NewGraph(b *testing.B) {
+	for _, numRevisions := range []int{smallNumRevisions, mediumNumRevisions, largeNumRevisions} {
+		for _, dataSize := range []int{smallDataSize, mediumDataSize, largeDataSize} {
+			b.Run(fmt.Sprintf("numRevisions=%d,dataSize=%d", numRevisions, dataSize), func(b *testing.B) {
+				for i := 0; i < b.N; i++ {
+					var graph Graph
+					for j := 0; j < numRevisions; j++ {
+						revision := types.NewRandomRevision(j, dataSize)
+						graph.AddVertex(revision)
+					}
+				}
+			})
+		}
 	}
 }
