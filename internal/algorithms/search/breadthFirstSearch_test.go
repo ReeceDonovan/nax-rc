b6ede3fb8123a7db2688d8aff3b8036fc1b76f72
Reece Donovan <reecedonovan99@gmail.com>
2023-04-30 06:38:27 +0100

    Updated breadthFirstSearch tests

--- internal/algorithms/search/breadthFirstSearch_test.go
+++ internal/algorithms/search/breadthFirstSearch_test.go
@@ -4,40 +4,61 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
+
+	"github.com/ReeceDonovan/nax-rc/internal/dataStructures/graph"
+	"github.com/ReeceDonovan/nax-rc/internal/types"
 )
 
-//	var DATA_SIZE = []struct {
-//		dataSize int
-//	}{
-//
-//		{100},
-//		{1000},
-//		{10000},
-//		{100000},
-//		{1000000},
-//	}
-func BenchmarkBreadthFirstSearch(b *testing.B) {
-	for _, dataSize := range DATA_SIZE {
-		b.Run(fmt.Sprintf("BreadthFirstSearch_%d", dataSize.dataSize), func(b *testing.B) {
-			dataSize := dataSize.dataSize
-			// g, revisions := createBigGraph(dataSize)
-			for i := 0; i < b.N; i++ {
-				g, revisions := createBigGraph(dataSize)
-				// select a random target revision
-				targetRevisionID := rand.Intn(26) + 65
-				var targetRevision = revisions[fmt.Sprintf("%c", targetRevisionID)]
-				if targetRevision == nil {
-					b.Errorf("expected target revision to not be nil, %v", targetRevisionID)
+func TestBreadthFirstSearch(t *testing.T) {
+	dag := graph.NewDAG()
+
+	nodes := make([]*graph.DAGNode, 10)
+	for i := 0; i < 10; i++ {
+		nodes[i], _ = dag.AddNode(types.NewBlankRevision(i))
+	}
+
+	for i := 0; i < 9; i++ {
+		_ = dag.AddEdge(i, i+1)
+	}
+
+	visitedNodes := make([]int, 0)
+	visitor := func(node *graph.DAGNode) {
+		visitedNodes = append(visitedNodes, node.Revision.ID())
+	}
+
+	BreadthFirstSearch(dag, 0, visitor)
+
+	expectedOrder := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	for i, nodeID := range visitedNodes {
+		if nodeID != expectedOrder[i] {
+			t.Errorf("Unexpected order of visited nodes, expected %v, got %v", expectedOrder, visitedNodes)
+			break
+		}
+	}
+}
+
+func BenchmarkBreadthFirstSearch_RevisionSearch(b *testing.B) {
+	for _, numRevisions := range []int{smallNumRevisions, mediumNumRevisions, largeNumRevisions} {
+		for _, dataSize := range []int{smallDataSize, mediumDataSize, largeDataSize} {
+			b.Run(fmt.Sprintf("numRevisions=%d,dataSize=%d", numRevisions, dataSize), func(b *testing.B) {
+				dag := graph.NewDAG()
+
+				nodes := make([]*graph.DAGNode, numRevisions)
+				for i := 0; i < numRevisions; i++ {
+					nodes[i], _ = dag.AddNode(types.NewRandomRevision(i, dataSize))
 				}
 
-				result := BreadthFirstSearch(g, revisions["A"], targetRevision.ID())
-
-				if result == nil {
-					b.Errorf("BreadthFirstSearch(%v)", targetRevision.ID())
+				for i := 0; i < numRevisions-1; i++ {
+					_ = dag.AddEdge(i, i+1)
 				}
 
-				// b.Logf("Target ID: %v, Result ID: %v", targetRevision.ID(), result.ID())
-			}
-		})
+				visitor := func(node *graph.DAGNode) {}
+
+				b.ResetTimer()
+				for i := 0; i < b.N; i++ {
+					BreadthFirstSearch(dag, rand.Intn(numRevisions), visitor)
+				}
+			})
+		}
 	}
 }
