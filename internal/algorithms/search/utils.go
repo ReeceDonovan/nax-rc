01efc5e71ebe2dca7e173821808027d426727da3
Reece Donovan <reecedonovan99@gmail.com>
2023-04-30 06:36:20 +0100

    Moved numRevisions and dataSizes constants to seperate file

---
+++ internal/algorithms/search/utils.go
@@ -0,0 +1,10 @@
+package search
+
+const (
+	smallNumRevisions  = 200
+	mediumNumRevisions = 2000
+	largeNumRevisions  = 20000
+	largeDataSize      = 10000 // 10 Kb
+	mediumDataSize     = 1000  // 1 Kb
+	smallDataSize      = 100   // 100 bytes
+)
