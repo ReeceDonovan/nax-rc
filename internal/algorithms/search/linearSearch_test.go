3b3654dbb4e4d2ad9627b6609023c32afd836159
Reece Donovan <reecedonovan99@gmail.com>
2023-04-30 06:37:09 +0100

    Updated linearSearch tests

--- internal/algorithms/search/linearSearch_test.go
+++ internal/algorithms/search/linearSearch_test.go
@@ -1,117 +1,67 @@
 package search
 
 import (
+	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/ReeceDonovan/nax-rc/internal/dataStructures/linkedList"
 	"github.com/ReeceDonovan/nax-rc/internal/types"
+	"github.com/stretchr/testify/assert"
 )
 
-const (
-	smallNumRevisions  = 20
-	mediumNumRevisions = 200
-	largeNumRevisions  = 2000
-	largeDataSize      = 1000000 // 1 Mb
-	mediumDataSize     = 1000    // 1 Kb
-	smallDataSize      = 100     // 100 bytes
-)
-
-func BenchmarkLinearSearch_SmallListSmallData(b *testing.B) {
-	list := createList(smallNumRevisions, smallDataSize)
-	targetID := smallNumRevisions - 1
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		LinearSearch(list, targetID)
+func createTestList() *linkedList.DoublyLinkedList {
+	list := linkedList.NewDoublyLinkedList()
+	for i := 0; i < 10; i++ {
+		revision := types.NewBlankRevision(i)
+		list.InsertAtPosition(i+1, &linkedList.DLLNode{Revision: revision})
 	}
+	return list
 }
 
-func BenchmarkLinearSearch_SmallListMediumData(b *testing.B) {
-	list := createList(smallNumRevisions, mediumDataSize)
-	targetID := smallNumRevisions - 1
+func TestLinearSearch(t *testing.T) {
+	list := createTestList()
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		LinearSearch(list, targetID)
+	for i := 0; i < 10; i++ {
+		node := LinearSearch(list, i)
+		assert.NotNil(t, node, "Node with ID %d not found", i)
+		assert.Equal(t, i, node.Revision.ID())
 	}
+
+	node := LinearSearch(list, -1)
+	assert.Nil(t, node, "Node with ID -1 should not be found")
 }
 
-func BenchmarkLinearSearch_SmallListLargeData(b *testing.B) {
-	list := createList(smallNumRevisions, largeDataSize)
-	targetID := smallNumRevisions - 1
-
+func BenchmarkLinearSearch(b *testing.B) {
+	list := createTestList()
 	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
-		LinearSearch(list, targetID)
+		revisionID := rand.Intn(10)
+		LinearSearch(list, revisionID)
 	}
 }
-
-func BenchmarkLinearSearch_MediumListSmallData(b *testing.B) {
-	list := createList(mediumNumRevisions, smallDataSize)
-	targetID := mediumNumRevisions - 1
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		LinearSearch(list, targetID)
-	}
-}
-
-func BenchmarkLinearSearch_MediumListMediumData(b *testing.B) {
-	list := createList(mediumNumRevisions, mediumDataSize)
-	targetID := mediumNumRevisions - 1
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		LinearSearch(list, targetID)
-	}
-}
-
-func BenchmarkLinearSearch_MediumListLargeData(b *testing.B) {
-	list := createList(mediumNumRevisions, largeDataSize)
-	targetID := mediumNumRevisions - 1
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		LinearSearch(list, targetID)
-	}
-}
-
-func BenchmarkLinearSearch_LargeListSmallData(b *testing.B) {
-	list := createList(largeNumRevisions, smallDataSize)
-	targetID := largeNumRevisions - 1
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		LinearSearch(list, targetID)
-	}
-}
-
-func BenchmarkLinearSearch_LargeListMediumData(b *testing.B) {
-	list := createList(largeNumRevisions, mediumDataSize)
-	targetID := largeNumRevisions - 1
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		LinearSearch(list, targetID)
-	}
-}
-
-func BenchmarkLinearSearch_LargeListLargeData(b *testing.B) {
-	list := createList(largeNumRevisions, largeDataSize)
-	targetID := largeNumRevisions - 1
-
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		LinearSearch(list, targetID)
-	}
-}
-
-func createList(numRevisions, dataSize int) *linkedList.DoublyLinkedList {
+func createListWithRevisions(numRevisions, dataSize int) *linkedList.DoublyLinkedList {
 	list := linkedList.NewDoublyLinkedList()
 	for i := 0; i < numRevisions; i++ {
 		revision := types.NewRandomRevision(i, dataSize)
-		node := &linkedList.Node{Revision: revision}
-		list.SetTail(node)
+		list.InsertAtPosition(i+1, &linkedList.DLLNode{Revision: revision})
 	}
 	return list
+}
+
+func BenchmarkLinearSearch_RevisionSearch(b *testing.B) {
+	for _, numRevisions := range []int{smallNumRevisions, mediumNumRevisions, largeNumRevisions} {
+		for _, dataSize := range []int{smallDataSize, mediumDataSize, largeDataSize} {
+			b.Run(fmt.Sprintf("numRevisions=%d,dataSize=%d", numRevisions, dataSize), func(b *testing.B) {
+				list := createListWithRevisions(numRevisions, dataSize)
+				b.ResetTimer()
+
+				for i := 0; i < b.N; i++ {
+					revisionID := rand.Intn(numRevisions)
+					LinearSearch(list, revisionID)
+				}
+			})
+		}
+	}
 }
