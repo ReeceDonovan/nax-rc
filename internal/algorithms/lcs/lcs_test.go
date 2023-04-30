e7b1c5d522372384f53a5907c3a5330b0471b698
Reece Donovan <reecedonovan99@gmail.com>
2023-04-30 06:41:38 +0100

    Updated longestCommonSubsequence tests

--- internal/algorithms/lcs/lcs_test.go
+++ internal/algorithms/lcs/lcs_test.go
@@ -5,44 +5,6 @@ import (
 	"testing"
 )
 
-/*
-Test Case 1:
-	"str1": "ZXVVYZW",
-	"str2": "XKYKZPW",
-
-Test Case 2:
-	"str1": "",
-	"str2": "",
-
-Test Case 3:
-	"str1": "",
-	"str2": "ABCDEFG",
-
-Test Case 4:
-	"str1": "ABCDEFG",
-	"str2": "",
-
-Test Case 5:
-	"str1": "ABCDEFG",
-	"str2": "ABCDEFG",
-
-Test Case 6:
-	"str1": "ABCDEFG",
-	"str2": "APPLES",
-
-Test Case 7:
-	"str1": "8111111111111111142",
-	"str2": "222222222822222222222222222222433333333332",
-
-Test Case 8:
-	"str1": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-	"str2": "CCCDDEGDHAGKGLWAJWKJAWGKGWJAKLGGWAFWLFFWAGJWKAG",
-
-Test Case 9:
-	"str1": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-	"str2": "CCCDDEGDHAGKGLWAJWKJAWGKGWJAKLGGWAFWLFFWAGJWKAGTUV",
-*/
-
 // Create a struct to hold the two input strings for each test case.
 type TestCase struct {
 	str1 string
