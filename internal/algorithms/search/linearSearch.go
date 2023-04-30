8dd83964bb442d3f5fb1f98ceb6638b9784b2389
Reece Donovan <reecedonovan99@gmail.com>
2023-04-30 06:36:55 +0100

    Updated linearSearch

--- internal/algorithms/search/linearSearch.go
+++ internal/algorithms/search/linearSearch.go
@@ -5,7 +5,7 @@ import (
 )
 
 // LinearSearch takes a doubly linked list and a revision ID, and returns the node containing the revision with the given ID or nil if not found.
-func LinearSearch(list *linkedList.DoublyLinkedList, revisionID int) *linkedList.Node {
+func LinearSearch(list *linkedList.DoublyLinkedList, revisionID int) *linkedList.DLLNode {
 	currentNode := list.Head
 	for currentNode != nil {
 		if currentNode.Revision.ID() == revisionID {
